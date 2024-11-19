b7fa129c62173b3561bc80ab2a105be02163522d
alastaira <alastair.aitchison@gmail.com>
2024-11-19 16:01:16 +0000

    Added missing PN5180 commands

--- PN5180.cpp
+++ PN5180.cpp
@@ -22,21 +22,6 @@
 #include "PN5180.h"
 #include "Debug.h"
 
-// PN5180 1-Byte Direct Commands
-// see 11.4.3.3 Host Interface Command List
-#define PN5180_WRITE_REGISTER           (0x00)
-#define PN5180_WRITE_REGISTER_OR_MASK   (0x01)
-#define PN5180_WRITE_REGISTER_AND_MASK  (0x02)
-#define PN5180_READ_REGISTER            (0x04)
-#define PN5180_WRITE_EEPROM				(0x06)
-#define PN5180_READ_EEPROM              (0x07)
-#define PN5180_SEND_DATA                (0x09)
-#define PN5180_READ_DATA                (0x0A)
-#define PN5180_SWITCH_MODE              (0x0B)
-#define PN5180_LOAD_RF_CONFIG           (0x11)
-#define PN5180_RF_ON                    (0x16)
-#define PN5180_RF_OFF                   (0x17)
-
 uint8_t PN5180::readBuffer[508];
 
 PN5180::PN5180(uint8_t SSpin, uint8_t BUSYpin, uint8_t RSTpin, SPIClass& spi) :
--- PN5180.h
+++ PN5180.h
@@ -21,6 +21,34 @@
 
 #include <SPI.h>
 
+// PN5180 1-Byte Direct Commands
+// see 11.4.3.3 Host Interface Command List
+// https://www.nxp.com/docs/en/data-sheet/PN5180A0XX-C1-C2.pdf
+#define PN5180_WRITE_REGISTER           (0x00)
+#define PN5180_WRITE_REGISTER_OR_MASK   (0x01)
+#define PN5180_WRITE_REGISTER_AND_MASK  (0x02)
+#define WRITE_REGISTER_MULTIPLE 		(0x03)
+#define PN5180_READ_REGISTER            (0x04)
+#define READ_REGISTER_MULTIPLE			(0x05)
+#define PN5180_WRITE_EEPROM				(0x06)
+#define PN5180_READ_EEPROM              (0x07)
+#define WRITE_TX_DATA 					(0x08)
+#define PN5180_SEND_DATA                (0x09)
+#define PN5180_READ_DATA                (0x0A)
+#define PN5180_SWITCH_MODE              (0x0B)
+#define MIFARE_AUTHENTICATE 			(0x0C)
+#define EPC_INVENTORY 					(0x0D) 
+#define EPC_RESUME_INVENTORY 			(0x0E)
+#define EPC_RETRIEVE_INVENTORY_RESULT_SIZE (0x0F)
+#define EPC_RETRIEVE_INVENTORY_RESULT 	(0x10)
+#define PN5180_LOAD_RF_CONFIG           (0x11)
+#define UPDATE_RF_CONFIG 				(0x12)
+#define RETRIEVE_RF_CONFIG_SIZE 		(0x13)
+#define RETRIEVE_RF_CONFIG 				(0x14)
+// Reserved for future use				(0x15) 
+#define PN5180_RF_ON                    (0x16)
+#define PN5180_RF_OFF                   (0x17)
+
 // PN5180 Registers
 #define SYSTEM_CONFIG       (0x00)
 #define IRQ_ENABLE          (0x01)
@@ -39,7 +67,6 @@
 #define SYSTEM_STATUS       (0x24)
 #define TEMP_CONTROL        (0x25)
 #define AGC_REF_CONFIG		(0x26)
-
 
 // PN5180 EEPROM Addresses
 #define DIE_IDENTIFIER      (0x00)
